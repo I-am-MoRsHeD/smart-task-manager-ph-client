e12344d6602e9923fc56fad20413d47f71923f50
MoRsHeD <mdmorshed0187@gmail.com>
2025-11-22 13:04:47 +0600

    fixed bugs

--- components/modules/auth/register-form.tsx
+++ components/modules/auth/register-form.tsx
@@ -20,7 +20,7 @@ const RegisterForm = () => {
     return (
         <form action={formAction}>
             <FieldGroup>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 gap-4">
                     {/* name */}
                     <Field>
                         <FieldLabel htmlFor="name">
